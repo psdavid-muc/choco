eedc7acb939d783221e588c4e9ee824d9846fb43
Peter S. David <peter.david@markt.de>
2017-07-18 16:46:58 +0200

    fixed hash error, updated package

--- activaid/tools/chocolateyinstall.ps1
+++ activaid/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   softwareName  = 'activaid*'
 
   checksum      = '8a6175efd2ecaab6c4d2e1e1b24092b6'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
 
   silentArgs   = '/S /D=%programfiles(x86)%\activAid'
   validExitCodes= @(0)
